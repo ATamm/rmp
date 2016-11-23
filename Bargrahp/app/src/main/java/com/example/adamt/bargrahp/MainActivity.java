e5005bbcd88b4dc692755df03ae71a1c3b8dd7db
Adam Tammariello <Adam Tammariello>
2016-11-23 16:17:28 -0700

    checking in bargraph logic

--- Bargrahp/app/src/main/java/com/example/adamt/bargrahp/MainActivity.java
+++ Bargrahp/app/src/main/java/com/example/adamt/bargrahp/MainActivity.java
@@ -8,45 +8,95 @@ import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     BarChart barChart;
-
-
+    ArrayList<String> dates;
+    ArrayList<BarEntry> barEntries;
+    Random random;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         barChart = (BarChart) findViewById(R.id.testGraph);
-        ArrayList<BarEntry> barEntries = new ArrayList<>();
-        barEntries.add(new BarEntry(44f, 0));
-        barEntries.add(new BarEntry(23f, 1));
-        barEntries.add(new BarEntry(10f, 2));
-        barEntries.add(new BarEntry(22f, 3));
-        barEntries.add(new BarEntry(50f, 4));
-        barEntries.add(new BarEntry(30f, 5));
-        barEntries.add(new BarEntry(11f, 6));
 
-        BarDataSet set = new BarDataSet(barEntries,"WeeklyUsage");
+        createRandomBarGraph( "2016/11/21", "2016/11/27");
 
-        ArrayList<String> weekDays = new ArrayList<>();
-        weekDays.add("Sunday");
-        weekDays.add("Monnday");
-        weekDays.add("Tuesday");
-        weekDays.add("Wednesday");
-        weekDays.add("Thursday");
-        weekDays.add("Friday");
-        weekDays.add("Saturday");
 
-        BarData data = new BarData(set);
-        data.setBarWidth(0.9f); // set custom bar width
-        barChart.setData(data);
-        barChart.setFitBars(true); // make the x-axis fit exactly all bars
-        barChart.invalidate(); // refresh
+
+
 
     }
 
+    public void createRandomBarGraph(String Date1, String Date2){
 
-}
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+
+            try {
+                Date date1 = simpleDateFormat.parse(Date1);
+                Date date2 = simpleDateFormat.parse(Date2);
+
+                Calendar mDate1 = Calendar.getInstance();
+                Calendar mDate2 = Calendar.getInstance();
+                mDate1.clear();
+                mDate2.clear();
+
+                mDate1.setTime(date1);
+                mDate2.setTime(date2);
+
+                dates = new ArrayList<>();
+                dates = getList(mDate1,mDate2);
+
+                barEntries = new ArrayList<>();
+                float max = 0f;
+                float value = 0f;
+                random = new Random();
+                for(int j = 0; j< dates.size();j++){
+                    max = 100f;
+                    value = random.nextFloat()*max;
+                    barEntries.add(new BarEntry(value,j));
+                }
+
+            }catch(ParseException e){
+                e.printStackTrace();
+            }
+
+            BarDataSet barDataSet = new BarDataSet(barEntries,"Dates");
+            BarData barData = new BarData(barDataSet);
+            barChart.setData(barData);
+
+
+        }
+    public ArrayList<String> getList(Calendar startDate, Calendar endDate){
+        ArrayList<String> list = new ArrayList<String>();
+        while(startDate.compareTo(endDate)<=0){
+            list.add(getDate(startDate));
+            startDate.add(Calendar.DAY_OF_MONTH,1);
+        }
+        return list;
+    }
+    public String getDate(Calendar cld){
+        String curDate = cld.get(Calendar.YEAR) + "/" + (cld.get(Calendar.MONTH) + 1) + "/"
+                +cld.get(Calendar.DAY_OF_MONTH);
+        try{
+            Date date = new SimpleDateFormat("yyyy/MM/dd").parse(curDate);
+            curDate =  new SimpleDateFormat("yyyy/MM/dd").format(date);
+        }catch(ParseException e){
+            e.printStackTrace();
+        }
+        return curDate;
+    }
+        }
+
+
+
+
+
